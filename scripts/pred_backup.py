36e4847e232c9edf34b1a445bd051ad807e06a18
dean <eastbright99@gmail.com>
2026-03-05 22:48:04 -0500

    update

--- scripts/pred_backup.py
+++ scripts/pred_backup.py
@@ -14,18 +14,18 @@ INPUT_DIM = 3       # px, py, angle
 OUTPUT_DIM = 3      # dx, dy, dtheta
 HIDDEN_DIMS = [256, 128, 64]
 
-LR_INNER = 0.05     # alpha, adaptation step size
+LR_INNER = 0.02     # alpha, adaptation step size
 LR_OUTER = 5e-4     # beta, meta step size
-INNER_STEP = 5      # 1-5, should be small
+INNER_STEP = 3      # 1-5, should be small
 BATCH_TASKS = 8     # number of tasks per meta step
 META_EPOCHS = 1000
 
 WEIGHT_DECAY = 1e-4
-DROPOUT_RATE = 0.1
+DROPOUT_RATE = 0.0
 
-FINETUNE_STEPS = 100
-FINETUNE_LR = 0.05
-FINETUNE_KSHOT = 100
+FINETUNE_STEPS = 200
+FINETUNE_LR = 0.03
+FINETUNE_KSHOT = 50
 
 NOVEL_OBJ = "mustard_bottle_flipped"
 
@@ -46,9 +46,8 @@ class Tasks:
         ("tuna_fish_can",   "data/x_tuna_fish_can_1000.npy",           "data/y_tuna_fish_can_1000.npy"),
         # ("mustard_bottle",  "data/x_mustard_bottle_flipped_1000.npy",  "data/y_mustard_bottle_flipped_1000.npy"),
         ("cracker_box",     "data/x_cracker_box_flipped_1000.npy",     "data/y_cracker_box_flipped_1000.npy"),
-        
     ]
-    
+
     Dts = (
         NOVEL_OBJ,  f"data/x_{NOVEL_OBJ}_1000.npy",  f"data/y_{NOVEL_OBJ}_1000.npy"
     )
@@ -165,67 +164,65 @@ class MAML:
         self.val_losses = []
 
     def inner_loop(self, xs, ys, meta_params):   # adaptation, alg 2-7
-        adapted_param = [mp.clone() for mp in meta_params]
+        adapted_params = [mp.clone() for mp in meta_params]
         for i in range(INNER_STEP):
-            ys_pred = self.model.functional_forward(xs, adapted_param, training=True)    # fpxj
-            loss = self.loss_fn(ys_pred, ys)
+            ys_pred = self.model.functional_forward(xs, adapted_params, training=True)    # fpxj
+            support_loss = self.loss_fn(ys_pred, ys)
             grads = torch.autograd.grad(
-                loss, adapted_param,
+                support_loss,
+                adapted_params,
                 create_graph=SECOND_ORDER,
-                allow_unused=True,
             )
-            adapted_param = [
-                mp - LR_INNER * (g if g is not None else torch.zeros_like(mp))
-                for mp, g in zip(adapted_param, grads)
+            adapted_params = [
+                mp - LR_INNER * g for mp, g in zip(adapted_params, grads)
             ]
+        with torch.no_grad():
+            ys_pred_final = self.model.functional_forward(xs, adapted_params, training=False)
+            final_support_loss = self.loss_fn(ys_pred_final, ys)
 
-        return adapted_param
-
+        return adapted_params, final_support_loss.item()
+    
     def meta_step(self):    # meta-learning, alg 2-10
         meta_params = list(self.model.parameters())
-        meta_loss = torch.zeros(1, device=self.device)
+        meta_loss = torch.zeros((), device=self.device)
+        support_loss_total = 0.0
         task_indices = self.tasks.sample_task_indices(BATCH_TASKS)
         for i in task_indices:
             xs, ys, xq, yq = self.tasks.sample_task(i, self.device)
-            adapted_param = self.inner_loop(xs, ys, meta_params)
-            yq_pred = self.model.functional_forward(xq, adapted_param, training=False)
-            meta_loss = meta_loss + self.loss_fn(yq_pred, yq)
+            adapted_params, support_loss = self.inner_loop(xs, ys, meta_params)   # support
+            support_loss_total += support_loss
+            yq_pred = self.model.functional_forward(xq, adapted_params, training=False)  # query
+            query_loss = self.loss_fn(yq_pred, yq)
+            meta_loss += query_loss
         meta_loss = meta_loss / len(task_indices)
+        support_loss_total = support_loss_total / len(task_indices)
         self.meta_opt.zero_grad()
         meta_loss.backward()
         torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
         self.meta_opt.step()
 
-        return meta_loss.item()
-    
-    @torch.no_grad()
-    def val_loss(self):
-        self.model.eval()
-        pred = self.model(self.tasks.x_test.to(self.device))
-        val_loss = self.loss_fn(pred, self.tasks.y_test.to(self.device))
-        self.model.train()
-
-        return val_loss.item()
+        return support_loss_total, meta_loss.item()
     
     def train(self):
         best_val = float('inf')
         for epoch in range(1, META_EPOCHS + 1):
             self.model.train()
-            train_loss = self.meta_step()
-            val_loss = self.val_loss()
+            support_loss, query_loss = self.meta_step()
 
-            self.train_losses.append(train_loss)
-            self.val_losses.append(val_loss)
+            self.train_losses.append(support_loss)
+            self.val_losses.append(query_loss)
 
-            if val_loss < best_val:
-                best_val = val_loss
+            if query_loss < best_val:
+                best_val = query_loss
                 torch.save(self.model.state_dict(), "best_maml.pth")
             
             if epoch % 50 == 0 or epoch == 1:
-                print(f"[{epoch:4d}/{META_EPOCHS} | meta-loss: {train_loss:.6f} | val-loss: {val_loss:.6f}]")
+                print(f"[{epoch:4d}/{META_EPOCHS} | tr-loss: {support_loss:.6f} | val-loss: {query_loss:.6f}]")
         print(f"\nBest val loss: {best_val:.6f}")
         self.plot_loss_curves()
 
+    # offline evaluation currently
+    # can update with online adaptation with live push data collecting
     def evaluate(self,
                  finetune_steps=FINETUNE_STEPS,
                  finetune_lr=FINETUNE_LR,
@@ -242,15 +239,15 @@ class MAML:
         self.model.eval()
         with torch.no_grad():
             loss_0 = self.loss_fn(self.model(xq), yq).item()
-        finetune = copy.deepcopy(self.model).train()
+        finetune = copy.deepcopy(self.model).train()    # original meta-model
         opt = torch.optim.SGD(finetune.parameters(), lr=finetune_lr, weight_decay=WEIGHT_DECAY)
-        for i in range(finetune_steps):
+        for i in range(finetune_steps):     # training raw meta-model
             opt.zero_grad()
             self.loss_fn(finetune(xs), ys).backward()
             opt.step()
 
         finetune.eval()
-        with torch.no_grad():
+        with torch.no_grad():   # evaluation with trained meta-model
             y_pred_norm = finetune(xq)
         loss_ft = self.loss_fn(y_pred_norm, yq).item()
 
@@ -282,8 +279,8 @@ class MAML:
     def plot_loss_curves(self):
         epoch = np.arange(1, len(self.train_losses) + 1)
         plt.figure(figsize=(9, 5))
-        plt.plot(epoch, self.train_losses, label="meta-train")
-        plt.plot(epoch, self.val_losses, label="val (0-shot)")
+        plt.plot(epoch, self.train_losses, label="support_loss")
+        plt.plot(epoch, self.val_losses, label="query_loss")
         plt.xlabel("Epoch"); plt.ylabel("MSE"); plt.title("MAML Loss")
         plt.legend(); plt.grid(alpha=0.3); plt.tight_layout()
         plt.savefig("data/maml_loss.png", dpi=150)
